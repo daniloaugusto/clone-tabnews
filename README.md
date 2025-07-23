d9a618d362fd7e30a21fd2e484a541ad6da453b1
Danilo Augusto <daniloaugustogrs@gmail.com>
2025-07-23 17:22:31 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 This project is a clone of the https://www.tabnews.com.br/ for learning purpose only.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olha ai, Naza, estou estudando umas coisas novas</h1>
+  return <h1>Olha ai, Naza, estou estudando umas coisas novas</h1>;
 }
 
 export default Home;
