81628bff0066af6f0926f5e152d63f799efcde8e
Danilo Augusto <daniloaugustogrs@gmail.com>
2025-08-21 11:12:52 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const updatedAt = responseBody.updated_at;
+  expect(updatedAt).toBeDefined();
+
+  const parseUpdatedAt = new Date(updatedAt).toISOString();
+  expect(updatedAt).toBe(parseUpdatedAt);
+
+  const database = responseBody.dependencies.database;
+  expect(database.version).toEqual("16.9");
+  expect(database.max_connections).toEqual(100);
+  expect(database.opened_connections).toEqual(1);
 });
